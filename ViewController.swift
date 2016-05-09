762e7f46d5b99c2cb3a30c8d11e0f4051481eab5
hmiranda3 <habibmiranda@gmail.com>
2016-05-08 21:29:41 -0600

    Interactive Story
    Fun story app with alternate endings.

---
+++ ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by Habib Miranda on 5/3/16.
+//  Copyright © 2016 littleJohns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "startAdventure" {
+            if let pageController = segue.destinationViewController as? PageController {
+                pageController.page = Adventure.story
+            }
+        }
+    }
+
+
+}
+
